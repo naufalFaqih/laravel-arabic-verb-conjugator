a0eb7dc4b29b01a2ae9b4c2e4d2c9642d9fd4ff8
naufalFaqih <naufalfaqih460@gmail.com>
2025-01-21 18:03:35 +0700

    add initial views and layout components for blog, home, about, and contact pages

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', ['title' => 'Home Page']);
+});
+Route::get('/about', function () {
+    return view('about', ['title' => 'About Page']);
+});
+
+//buat 2 route baru untuk blog dan kontak
+Route::get('/blog', function () {
+    return view('blog', ['title' => 'Blog Page']);
+});
+Route::get('/kontak', function () {
+    return view('kontak', ['title' => 'Kontak Page']);
 });
